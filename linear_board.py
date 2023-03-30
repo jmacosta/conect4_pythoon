1148df653376fb3b935b2cc3c5840a079e1d9768
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-03-30 10:15:24 +0200

    First test passed

--- linear_board.py
+++ linear_board.py
@@ -1,2 +1,27 @@
+from settings import *
+
+
 class LinearBoard():
-    pass
+    board = []
+
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        return "tablero vacio"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def is_full(self):
+        if (len(self.board) < BOARD_LENGTH):
+            return False
+
+    def add(self, token):
+        self.board.append(token)
+
+    def is_tie(self, playerA, playerB):
+        pass
+
+    def is_victory(self, player):
+        return False
